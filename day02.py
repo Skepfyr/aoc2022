c69b16f4f0f25eea83ec220474613322d22ea075
Jack Rickard <jack.rickard@outlook.com>
2022-12-03 08:21:47 +0000

    Day 2

---
+++ day02.py
@@ -0,0 +1,19 @@
+import sys
+
+part1_total = 0
+part2_total = 0
+for line in sys.stdin:
+    [opponent, me] = line.split(" ")
+    opponent = ord(opponent.strip()) - ord('A')
+    me = ord(me.strip()) - ord('X')
+    outcome = (me - opponent) % 3
+    score = me + 1 + [3, 6, 0][outcome]
+    part1_total += score
+
+    desired_outcome = me - 1
+    best_play = (desired_outcome + opponent) % 3
+    score = best_play + 1 + [3, 6, 0][desired_outcome]
+    part2_total += score
+
+print(f"Part 1: {part1_total}")
+print(f"Part 2: {part2_total}")
